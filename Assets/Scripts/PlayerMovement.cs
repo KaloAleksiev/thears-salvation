a2aa6012f980475faaaea7c780c8dcc9c5887506
Madzhunov,Kaloyan K.M <k.madzhunov@student.fontys.nl>
2021-02-28 22:50:44 +0200

    prevent player from jumping if the character is not on the ground

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,13 +8,15 @@ public class PlayerMovement : MonoBehaviour {
     public Rigidbody2D rb; // used to add velocity and forces to the player
 
     public float jumpForce = 20f;
+    public Transform feet;
+    public LayerMask groundLayers;
 
     private float mx; // movement on the x-axis
 
     private void Update() {
         mx = Input.GetAxis("Horizontal"); // set the movement on the x-axis to what the player inputs (A, D, Left Arrow Key, Right Arrow Key)
 
-        if (Input.GetButtonDown("Jump")) {
+        if (Input.GetButtonDown("Jump") && IsGrounded()) {
             Jump();
         }
     }
@@ -29,5 +31,11 @@ public class PlayerMovement : MonoBehaviour {
         Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
 
         rb.velocity = movement;
+    }
+
+    public bool IsGrounded() {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
+
+        return groundCheck != null;
     }
 }
