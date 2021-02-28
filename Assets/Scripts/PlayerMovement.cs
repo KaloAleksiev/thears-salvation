a9477e5e046bfe5c334a4ed76592e15596eaa084
Madzhunov,Kaloyan K.M <k.madzhunov@student.fontys.nl>
2021-02-28 21:37:32 +0200

    Create player movement script and attach it to the player object

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float movementSpeed;
+    public Rigidbody2D rb; // used to add velocity and forces to the player
+
+    private float mx; // movement on the x-axis
+
+    private void Update() {
+        mx = Input.GetAxis("Horizontal"); // set the movement on the x-axis to what the player inputs (A, D, Left Arrow Key, Right Arrow Key)
+    }
+
+    private void FixedUpdate() {
+        Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
+
+        rb.velocity = movement;
+    }
+}
