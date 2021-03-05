4fd437c260a04cbed836a6778e7d4fb09a9c9a5f
Madzhunov,Kaloyan K.M <k.madzhunov@student.fontys.nl>
2021-03-05 11:58:31 +0200

    change logic of double jump mechanic and restructure code

--- Kaloyan Madzhunov/01_Simple_2D_Platformer/Assets/Scripts/PlayerMovement.cs
+++ Kaloyan Madzhunov/01_Simple_2D_Platformer/Assets/Scripts/PlayerMovement.cs
@@ -4,52 +4,62 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-    public float movementSpeed;
-    public Rigidbody2D rb; // used to add velocity and forces to the player
-
+    public float movementSpeed = 10f;
     public float jumpForce = 20f;
+    public int extraJumps = 1;
+
+    public Rigidbody2D rb; //used to add velocity and forces to the player
     public Transform feet;
-    public LayerMask groundLayers;
+    public LayerMask groundLayer;
 
-    private float mx; // movement on the x-axis
-
+    private float mx; //movement on the x-axis
     private int jumpCounter = 0;
+    private float jumpCoolDown;
+    private bool isGrounded = false;
 
     private void Update() {
-        mx = Input.GetAxis("Horizontal"); // set the movement on the x-axis to what the player inputs (A, D, Left Arrow Key, Right Arrow Key)
+        mx = Input.GetAxis("Horizontal"); //set the movement on the x-axis to what the player inputs (A, D, Left Arrow Key, Right Arrow Key)
 
-        if (Input.GetButtonDown("Jump") && jumpCounter < 1) {
+        rotateCharacter();
+
+        if (Input.GetButtonDown("Jump")) {
             Jump();
+        }
+
+        CheckGrounded();
+    }
+
+    private void FixedUpdate() {
+        rb.velocity = new Vector2(mx * movementSpeed, rb.velocity.y);
+    }
+
+    private void Jump() {
+        if (isGrounded || jumpCounter < extraJumps) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce); ;
             jumpCounter++;
         }
+    }
 
-        if (IsGrounded()) {
-            jumpCounter = 0;
+    public void CheckGrounded() {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayer); //returns true if the circle collides with the layer mask
+
+        if (groundCheck) { //on the ground
+            isGrounded = true;
+            jumpCounter = 0; //reset the jump count
+            jumpCoolDown = Time.time + 0.2f;
+        } else if (Time.time < jumpCoolDown) { //if the player is not touching the ground, give them the benefit of 0.2 seconds to hit the jump key
+            isGrounded = true;
+        } else { //the player is not touching the ground
+            isGrounded = false;
         }
+    }
 
-        // make character face left or right depending on key pressed
+    private void rotateCharacter() {
+        //make character face left or right depending on key pressed
         if (mx > 0f) {
             transform.localScale = new Vector3(1f, 1f, 1f);
         } else if (mx < 0f) {
             transform.localScale = new Vector3(-1f, 1f, 1f);
         }
-    }
-
-    private void FixedUpdate() {
-        Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
-
-        rb.velocity = movement;
-    }
-
-    private void Jump() {
-        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
-
-        rb.velocity = movement;
-    }
-
-    public bool IsGrounded() {
-        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
-
-        return groundCheck != null;
     }
 }
