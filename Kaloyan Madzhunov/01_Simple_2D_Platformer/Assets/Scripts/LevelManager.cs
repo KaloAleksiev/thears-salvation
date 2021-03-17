5eb1c9f5cde801d463dedc3bbd4754655291a13b
Madzhunov,Kaloyan K.M <k.madzhunov@student.fontys.nl>
2021-03-17 17:18:09 +0200

    add player death respawn functionality

---
+++ Kaloyan Madzhunov/01_Simple_2D_Platformer/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class LevelManager : MonoBehaviour {
+    public static LevelManager instance;
+    
+    public Transform respawnPoint;
+    public GameObject playerPrefab;
+
+    public CinemachineVirtualCameraBase cameraBase;
+
+    [Header("Soul Orbs")]
+    public int soulOrbs = 0;
+
+    private void Awake() {
+        instance = this;
+    }
+
+    public void Respawn() {
+        GameObject player = Instantiate(playerPrefab, respawnPoint.position, Quaternion.identity);
+        cameraBase.Follow = player.transform;
+    }
+
+    public void IncreaseSoulOrbs(int amount) {
+        soulOrbs += amount;
+    }
+}
---
+++ Kaloyan Madzhunov/01_Simple_2D_Platformer/Assets/Scripts/PlayerLifespan.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLifespan : MonoBehaviour {
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Enemy")) {
+            Destroy(gameObject);
+            LevelManager.instance.Respawn();
+        }
+    }
+}
