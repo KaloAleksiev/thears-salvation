0facfa0f7bed5f271ba9c5f80309b52f09d4dcfb
Madzhunov,Kaloyan K.M <k.madzhunov@student.fontys.nl>
2021-03-17 18:11:24 +0200

    make enemies die after certain amount of mouse clicks

---
+++ Kaloyan Madzhunov/01_Simple_2D_Platformer/Assets/Scripts/EnemyDeath.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeath : MonoBehaviour
+{
+    public int deathClicks = 1;
+    public int clicksCounter = 0;
+
+    private void OnMouseDown() {
+        if (++clicksCounter == deathClicks) {
+            Destroy(gameObject);
+        }
+    }
+}
