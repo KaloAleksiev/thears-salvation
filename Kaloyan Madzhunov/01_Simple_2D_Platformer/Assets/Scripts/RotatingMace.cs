787545655f3e0c723cd8a49e8d63bcb3fe0479ab
Madzhunov,Kaloyan K.M <k.madzhunov@student.fontys.nl>
2021-03-31 16:51:30 +0300

    add bool parameter to rotating spikes to determine the rotation direction

--- Kaloyan Madzhunov/01_Simple_2D_Platformer/Assets/Scripts/RotatingMace.cs
+++ Kaloyan Madzhunov/01_Simple_2D_Platformer/Assets/Scripts/RotatingMace.cs
@@ -2,8 +2,9 @@
 
 public class RotatingMace : MonoBehaviour {
     public float speed;
+    public bool isRotationClockwise = true;
 
     void FixedUpdate() {
-        transform.Rotate(Vector3.forward, speed);
+        transform.Rotate(Vector3.forward * (isRotationClockwise ? 1 : -1), speed);
     }
 }
