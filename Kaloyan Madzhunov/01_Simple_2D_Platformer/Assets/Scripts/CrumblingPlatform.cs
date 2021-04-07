59c2807a9f88372dfc23e87f7080b68df46c1f42
Madzhunov,Kaloyan K.M <k.madzhunov@student.fontys.nl>
2021-04-07 13:07:56 +0300

    crumbling platform script wip

---
+++ Kaloyan Madzhunov/01_Simple_2D_Platformer/Assets/Scripts/CrumblingPlatform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrumblingPlatform : MonoBehaviour {
+    /*private void Update() {
+        StartCoroutine("StartIdleBeforeDisappear");
+    }
+
+    IEnumerator StartIdleBeforeDisappear() {
+        for (float elapsedTime = 0f; elapsedTime < 2; elapsedTime += 1) {
+            yield return new WaitForSeconds(1);
+        }
+
+        yield return StartCoroutine(BreakPlatform(gameObject, 2f));
+    }
+
+    private IEnumerator BreakPlatform(GameObject platform, float resetTime) {
+        platform.GetComponent<BoxCollider2D>().enabled = false;
+        platform.GetComponent<SpriteRenderer>().enabled = false;
+
+        yield return new WaitForSeconds(resetTime);
+
+        platform.GetComponent<BoxCollider2D>().enabled = true;
+        platform.GetComponent<SpriteRenderer>().enabled = true;
+    }*/
+}
