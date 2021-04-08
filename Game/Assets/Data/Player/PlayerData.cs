e47a27e5374fdc21dbc397837fb45202c800d904
Madzhunov,Kaloyan K.M <k.madzhunov@student.fontys.nl>
2021-04-08 18:15:39 +0300

    create player and upgrades scriptable objects with data

---
+++ Game/Assets/Data/Player/PlayerData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Player", menuName = "Player")]
+public class PlayerData : ScriptableObject {
+    public int soulOrbs;
+
+    public void AddSoulOrbs(int amount) {
+        this.soulOrbs += amount;
+    }
+}
---
+++ Game/Assets/Data/Upgrades/Upgrade.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Upgrade", menuName = "Upgrade")]
+public class Upgrade : ScriptableObject {
+    public static string CostText = "Soul Orbs per upgrade";
+    public static string UpgradesDoneText = "upgrades done";
+
+    public Sprite icon;
+    public new string name;
+    public string description;
+    public int cost;
+    public int upgradesDone;
+    public int maxUpgrades;
+}
