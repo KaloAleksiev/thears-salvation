4cbfbcd008f844929267035601f59c0fae96c49d
Madzhunov,Kaloyan K.M <k.madzhunov@student.fontys.nl>
2021-04-19 13:31:46 +0300

    fix issue where the video in main menu is not played on webgl and change background color of camera in main menu

---
+++ Game/Assets/Scripts/HUD/Menus/VideoPlayerPath.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlayerPath : MonoBehaviour {
+    private VideoPlayer videoPlayer;
+
+    void Start() {
+        videoPlayer = gameObject.GetComponent<VideoPlayer>();
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "BackgroundVideo.mp4");
+    }
+}
