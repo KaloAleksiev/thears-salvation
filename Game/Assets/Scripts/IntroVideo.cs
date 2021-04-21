3c137cc63b091d710d814ca541643f8f93be8fed
Madzhunov,Kaloyan K.M <k.madzhunov@student.fontys.nl>
2021-04-21 22:17:04 +0300

    fix bug where video sound was off on desktop build and make game start when the intro video is over

---
+++ Game/Assets/Scripts/IntroVideo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class IntroVideo : MonoBehaviour {
+    private VideoPlayer videoPlayer;
+
+    private void Start() {
+        videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.loopPointReached += CheckOver;
+    }
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp) {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Game/Assets/Scripts/Player/Narration.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Narration : MonoBehaviour {
+    public Player player;
+    public TextMeshProUGUI[] textArray;
+
+    public bool[] textArrayActive;
+
+    private void Start() {
+        textArrayActive = new bool[textArray.Length];
+
+        for (int i = 0; i < textArrayActive.Length; i++) {
+            textArrayActive[i] = false;
+        }
+    }
+
+    private void Update() {
+        if (Input.anyKeyDown) {
+            if (!InGameMenus.GameIsPaused) {
+                FreezeGame(false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        NarrationElement narrationElement = collider.GetComponent<NarrationElement>();
+
+        if (narrationElement) {
+            if (!textArrayActive[narrationElement.number]) {
+                FreezeGame(true);
+
+                TextMeshProUGUI text = textArray[narrationElement.number];
+                text.gameObject.SetActive(true);
+                text.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 4, gameObject.transform.position.z);
+
+                if (narrationElement.number > 0) textArray[narrationElement.number - 1].gameObject.SetActive(false);
+                textArrayActive[narrationElement.number] = true;
+            }
+        }
+    }
+
+    private void FreezeGame(bool isGameFrozen) {
+        if (isGameFrozen) {
+            Time.timeScale = 0f;
+            player.GetComponent<Animator>().enabled = false;
+            player.playerMovement.enabled = false;
+            player.playerCombat.enabled = false;
+        } else {
+            Time.timeScale = 1f;
+            player.GetComponent<Animator>().enabled = true;
+            player.playerMovement.enabled = true;
+            player.playerCombat.enabled = true;
+        }
+    }
+}
